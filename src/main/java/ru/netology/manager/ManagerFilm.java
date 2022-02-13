3a94f32f753106c5cd324d0a1c06881f3c01ae2a
LaraKra <konlor@rambler.ru>
2022-02-13 21:34:24 +0300

    Iinitial commit

---
+++ src/main/java/ru/netology/manager/ManagerFilm.java
@@ -0,0 +1,37 @@
+package ru.netology.manager;
+
+public class ManagerFilm {
+    private int limit = 10;
+    private Film[] items = new Film[0];
+
+    public ManagerFilm() {
+    }
+
+    public ManagerFilm(int limit) {
+        this.limit = limit;
+    }
+
+    public void save(Film item) {
+        int length = items.length + 1;
+        Film[] tmp = new Film[length];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
+
+    }
+
+    public Film[] getAll() {
+        int resultLength = Math.min(items.length, limit);
+        Film[] result = new Film[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = items[items.length - i - 1];
+        }
+        return result;
+    }
+
+    public Film[] findAll() {
+        return items;
+    }
+
+}
